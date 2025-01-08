45b7a47260ad173c328ce7c448c58563dd23ab56
Paulo <pauloferadosgames@gmail.com>
2025-01-08 11:24:18 -0300

    adaptação do graphComponent para receber as linguas

--- src/components/GraphComponent/GraphComponent.jsx
+++ src/components/GraphComponent/GraphComponent.jsx
@@ -1,9 +1,8 @@
 import './style.css'
 import '../../App.css'
-export default function GraphComponent({}) {
+export default function GraphComponent({languages}) {
     return(
         <div className='container'>
-            
         </div>
     )
 }
